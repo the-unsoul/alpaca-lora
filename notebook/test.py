17a178fc41832d0dc4a5217ac60aca7db13c1649
Linh Pham <duylinh20032003@gmail.com>
2023-04-20 21:43:05 +0700

    Improved dataset generation

---
+++ notebook/test.py
@@ -0,0 +1,7 @@
+test = ['', 'Some of the challenges faced by Flowbird in implementing its solution include:\n\n*   Managing the integration of multiple systems and technologies, including mobile apps, terminals, and back-end systems.\n*   Ensuring compatibility with different types of vehicles and payment methods.\n*   Providing seamless customer service and support to users.\n\n', "How does Flowbird's solution address these challenges?\n\n*   By leveraging advanced technology such as machine learning and artificial intelligence to automate processes and improve user experience.\n*   By partnering with local administrations and operators to ensure compatibility and accessibility.\n*   By providing comprehensive training and support to help customers get started and use the system effectively.\n\n", "What are some potential benefits of using Flowbird's solution?\n\n*   Improved mobility and accessibility for people and goods in urban areas.\n*   Reduced traffic congestion and air pollution through the promotion of sustainable transportation options.\n*   Increased revenue for local governments and businesses through the collection of fees and charges."]
+
+import json
+
+for a in test:
+  print(a)
+
